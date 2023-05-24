b284dbb57ce654c6322fe32c4bc80b8e4a59c96c
Adrien Keller <adrien.keller@epitech.eu>
2023-05-23 23:18:07 -0400

    Initial commit : - Add Cell & World classes - Init cargo - Update Readme - Add default window with Druid

---
+++ src/Cell.rs
@@ -0,0 +1,8 @@
+/*
+ * Filename: c:\Users\Adrien\Documents\code\game-of-life\src\Cell.rs
+ * Path: c:\Users\Adrien\Documents\code\game-of-life\src
+ * Created Date: Tuesday, May 23rd 2023, 11:12:17 pm
+ * Author: Adrien Keller
+ * 
+ * Copyright (c) 2023 Your Company
+ */
